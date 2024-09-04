43466f6e93e7ba1a20b359585122462c9d72c707
Karanjeet-kumar <karanrajput8400@gmail.com>
2024-09-04 16:09:21 +0530

    Login-SignUp on Homepage Created

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from "react";
+import {
+  Container,
+  Box,
+  Text,
+  Tab,
+  Tabs,
+  TabList,
+  TabPanel,
+  TabPanels,
+} from "@chakra-ui/react";
+import Login from "../components/Authentication/Login";
+import SinUp from "../components/Authentication/SignUp";
+import SignUp from "../components/Authentication/SignUp";
 
 function Homepage() {
   return (
-    <div>Homepage</div>
-  )
+    <Container maxW={"xl"} centerContent>
+      <Box
+        display={"flex"}
+        justifyContent={"center"}
+        p={2}
+        bg={"white"}
+        w={"100%"}
+        m={"40px 0 15px 0"}
+        borderRadius={"lg"}
+        borderWidth={"1px"}
+      >
+        <Text fontSize={"3xl"} fontWeight={"1000"} fontFamily={"Work sans"}>
+          Talk - Pulse
+        </Text>
+      </Box>
+      <Box
+        p={4}
+        bg={"white"}
+        w={"100%"}
+        borderRadius={"lg"}
+        borderWidth={"1px"}
+      >
+        <Tabs variant="soft-rounded">
+          <TabList mb={"1em"}>
+            <Tab width={"50%"}>Login</Tab>
+            <Tab width={"50%"}>Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <SignUp />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
 }
 
-export default Homepage
+export default Homepage;
