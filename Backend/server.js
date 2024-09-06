7d11c89b233aa8550ec26f4aeb7fa9ff29b21c93
Karanjeet-kumar <karanrajput8400@gmail.com>
2024-09-06 23:33:11 +0530

    MongoDB to NodeJS connected

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connectDB = require("./config/database");
+const colors = require("colors");
 
-const app = express();
 dotenv.config();
+connectDB();
+const app = express();
 
-app.get('/', (req,res) => {
-    res.send("API is running.");
+app.get("/", (req, res) => {
+  res.send("API is running.");
 });
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, console.log(`Server started on port ${PORT}`));
+app.listen(PORT, console.log(`Server started on port ${PORT}`.yellow.bold));
